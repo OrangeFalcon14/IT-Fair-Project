c6c815375a238f6e7b46df001c0b3a056f71e933
Neel-coder777 <neel.theganahally@gmail.com>
2022-07-16 20:23:29 +0530

    SomeUpdates

--- F1Application (copy)/src/main/java/com/example/F1Application/Controller/ItFairProjectTTGameController.java
+++ F1Application (copy)/src/main/java/com/example/F1Application/Controller/ItFairProjectTTGameController.java
@@ -1,7 +1,16 @@
 package com.example.F1Application.Controller;
 
+import com.example.F1Application.Service.ItFairProjectTTGameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ItFairProjectTTGameController {
+
+    @Autowired
+    private ItFairProjectTTGameService itFairProjectTTGameService;
+
+    public ItFairProjectTTGameController(ItFairProjectTTGameService itFairProjectTTGameService) {
+        this.itFairProjectTTGameService = itFairProjectTTGameService;
+    }
 }
--- F1Application (copy)/src/main/java/com/example/F1Application/Service/ItFairProjectTTGameService.java
+++ F1Application (copy)/src/main/java/com/example/F1Application/Service/ItFairProjectTTGameService.java
@@ -1,7 +1,16 @@
 package com.example.F1Application.Service;
 
+import com.example.F1Application.Repository.ItFairProjectTTGameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItFairProjectTTGameService {
+
+    @Autowired
+    private ItFairProjectTTGameRepository itFairProjectTTGameRepository;
+
+    public ItFairProjectTTGameService(ItFairProjectTTGameRepository itFairProjectTTGameRepository) {
+        this.itFairProjectTTGameRepository = itFairProjectTTGameRepository;
+    }
 }
