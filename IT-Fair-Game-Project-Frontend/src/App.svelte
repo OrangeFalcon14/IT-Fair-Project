2eb7c14555cfe984394bba17e1255d4986e13b54
OrangeFalcon14 <vishrs020@gmail.com>
2022-11-05 09:15:29 +0530

    Add IQ test

--- IT-Fair-Game-Project-Frontend/src/App.svelte
+++ IT-Fair-Game-Project-Frontend/src/App.svelte
@@ -2,6 +2,7 @@
     import Welcome from "./views/Welcome.svelte";
     import Typing from './views/Typing.svelte';
     import GKTest from "./views/GKTest.svelte";
+    import IQTest from "./views/IQTest.svelte";
 
     let views = ["welcome", "typing", "gktest", "optical_illusion", "iq_test"];
     let currentTest = 0;
@@ -17,6 +18,8 @@
         <Typing on:click={next_test} />
     {:else if currentTest == 2}
         <GKTest on:click={next_test}/>
+    {:else if currentTest == 3}
+        <IQTest on:click={next_test} />
     {/if}
 </div>
 
